c878c9faddb127df1fbfcd80d4ecbf8f2344c488
Tomohiro Meo <h2.adachi.tomohiro@gmail.com>
2021-03-02 14:21:30 -0800

    Tmp commit

---
+++ components/toolbar.js
@@ -0,0 +1,14 @@
+import { useRouter } from 'next/router';
+import styles from '../styles/Toolbar.module.css';
+
+export const Toolbar = () => {
+	const router = useRouter();
+
+	return (
+		<div className={styles.main}>
+			<div onClick={() => router.push('/')}>Home</div>
+			<div onClick={() => router.push('/feed/1')}>Touch</div>
+			<div onClick={() => router.push('/eom')}>Dokaben</div>
+		</div>
+	);
+};
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,45 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import Layout from '../components/layout'
+import Head from 'next/head';
+import styles from '../styles/EOM.module.css';
+import { Toolbar } from '../components/toolbar';
 
-const Home = () => <Layout>Hello next!</Layout>;
+export const Home = () => {
+	return (
+		<>
+			<Head>
+				<title>I love Baseball "MANGE"</title>
+			</Head>
+			<div className="page-container">
+				<Toolbar />
+				<div className={styles.main}>
+					<h1>I love Baseball MANGE!</h1>
+					<h2>I highly recommend you the following two Baseball MANGE!</h2>
+					<div className={styles.employeeOfTheMonth}>
+						<div>
+							<h3>Touch</h3>
+							<img src='https://cdn.myanimelist.net//images//manga//5//84777.jpg' />
+						</div>
+						<div>
+							<h3>Dokaben</h3>
+							<img src='https://cdn.myanimelist.net//images//manga//2//136961.jpg?s=0c8f54c9e38e3032b192ed2a501acfbe' />
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+};
+
+// export const getServerSideProps = async pageContext => {
+// 	const apiResponse = await fetch(
+// 		'https://my-json-server.typicode.com/portexe/next-news/employeeOfTheMonth',
+// 	);
+// 	const employee = await apiResponse.json();
+//
+// 	return {
+// 		props: {
+// 			employee,
+// 		},
+// 	};
+// };
 
 export default Home;
